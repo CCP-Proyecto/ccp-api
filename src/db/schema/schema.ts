ef5f919553fde5194fce9ce5823842e1814d0e7d
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2025-04-06 16:37:20 -0500

    chore(manufacturer): update db schema

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -1,12 +1,12 @@
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const manufacturer = pgTable("manufacturer", {
-  id: text("id").primaryKey(),
+  id: text("id").primaryKey().notNull(),
   idType: text("id_type").notNull(),
   name: text("name").notNull(),
-  phone: text("phone"),
-  address: text("address"),
-  email: text("email"),
+  phone: text("phone").notNull(),
+  address: text("address").notNull(),
+  email: text("email").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
