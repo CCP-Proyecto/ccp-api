4284f6f8aa95dcccbf1c6c8470a5edfcb5f663ce
jjmontenegrop <jj.montenegro@uniandes.edu.co>
2025-04-23 21:47:52 -0500

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,5 +5,12 @@ import { auth } from "@/lib/auth";
 
 export const verifySession = createMiddleware(async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    throw new HTTPException(401, {
+      message: "Invalid or missing session",
+    });
+  }
+
   return next();
 });
