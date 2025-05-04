cb494497f5bebffb8c660e2b5e97f854d8c2bffe
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2025-05-04 14:10:03 -0500

    chore: debugging

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,6 +6,10 @@ import { auth } from "@/lib/auth";
 export const verifySession = createMiddleware(async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
+  console.log("*******************************************");
+  console.log(JSON.stringify(session));
+  console.log("*******************************************");
+
   if (!session) {
     throw new HTTPException(401, {
       message: "Invalid or missing session",
