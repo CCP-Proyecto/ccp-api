05fe7eeb849bf02cb05f08982212c667857e39d9
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2025-05-04 14:16:40 -0500

    chore: debugging

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,6 +4,9 @@ import { HTTPException } from "hono/http-exception";
 import { auth } from "@/lib/auth";
 
 export const verifySession = createMiddleware(async (c, next) => {
+  console.error("*******************************************");
+  console.error(JSON.stringify(c.req.raw.headers));
+  console.error("*******************************************");
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
   console.error("*******************************************");
