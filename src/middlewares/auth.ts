0cb9bcdbcde9dec803f3e685d415fd98be1b1074
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2025-05-04 14:25:15 -0500

    chore: remove debugging

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,14 +4,7 @@ import { HTTPException } from "hono/http-exception";
 import { auth } from "@/lib/auth";
 
 export const verifySession = createMiddleware(async (c, next) => {
-  console.error("*******************************************");
-  console.error(JSON.stringify(c.req.raw.headers));
-  console.error("*******************************************");
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
-
-  console.error("*******************************************");
-  console.error(JSON.stringify(session));
-  console.error("*******************************************");
 
   if (!session) {
     throw new HTTPException(401, {
